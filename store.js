f45a222b8f5d29c3f91d459d3eeac040f5335f76
JustWiseLove <justwiselove@icloud.com>
2026-08-11 05:22:54 -0400

    Update store.js

--- store.js
+++ store.js
@@ -52,6 +52,24 @@ const STORE = {
       unit: "mg/L",
       level: 27.9,
       ts: "2026-08-10T13:30:00.000Z"
+    },
+    {
+      id: "r005",
+      siteId: "shockley",
+      date: "2026-08-11",
+      clarity: 0.13,
+      unit: "g/L",
+      level: 49.2,
+      ts: "2026-08-11T09:30:00.000Z"
+    },
+    {
+      id: "r006",
+      siteId: "darkrun",
+      date: "2026-08-11",
+      clarity: 17,
+      unit: "mg/L",
+      level: 29.4,
+      ts: "2026-08-11T09:30:00.000Z"
     }
   ]
 };
